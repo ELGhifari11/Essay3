de0743479449bc82935f6852b1c7458d059cb7c5
EL Ghifari <121995729+ELGhifari11@users.noreply.github.com>
2023-02-18 12:02:18 +0700

    Add files via upload

---
+++ essay3.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <=20; i ++ ) {
+       if (i % 2 == 0) {
+        console.log(i + " - Berkualitas");
+    } if  ( i == 3 || i == 9 || i == 15 ) {
+        console.log(i + " - I love Coding ");
+    } else if (i % 2 == 1) {
+        console.log(i + " - Santai");
+    }
+}
